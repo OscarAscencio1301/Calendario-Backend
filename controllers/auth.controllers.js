0ca6be5e4ef107cd0f75d2720dfa28251b737a04
Oscar <oscareascencioalva@gmail.com>
2021-09-18 14:00:53 -0500

    Carpeta public

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -6,7 +6,6 @@ const validarJWT = require('../middlewares/validarJWT');
 const crearUsuario = async (req, resp) => {
     const { nombre, correo, password } = req.body
     const usuario = new Usuario({ nombre, correo, password })
-    console.log(usuario)
 
     const salt = bcryptjs.genSaltSync();
     usuario.password = bcryptjs.hashSync(password, salt);
